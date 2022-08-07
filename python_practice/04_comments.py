725a1ee349fea9d3592d5065295e663a07e42774
AhmarSaeed1011 <ahmarsaeed1011@gmail.com>
2022-08-07 10:09:17 +0500

    third commit

---
+++ python_practice/04_comments.py
@@ -0,0 +1,3 @@
+print("How are you?") #express the comment out
+# print("We are Learning python with Ammar")
+# print(2+6)
---
+++ python_practice/05_variables.py
@@ -0,0 +1,27 @@
+#varibales: objects containing specific values
+#x=5 #numeric or integer variable
+#print(x)
+
+#y="we are learning python with Ammar" #string variable
+#print(y)
+
+#x=x+15
+#print(x)
+
+#types of varibale
+#type(x)
+#print(type(y))
+#print_type_class
+
+#rules to assign a variable
+#1- the variables should contain letters, numbers or underscores
+#2- Do not with numbers
+#3- spaces are not allowed
+#4- Do not use keywords used in functions (break, mean, media, test etc)
+#5- short and descriptive
+#6- Case sensitivity (lowercase, uppercase letters, lowercase letters should be used)
+
+fruit_basket =8
+fruit_basket="Mangoes"
+print(type(fruit_basket))
+print(fruit_basket)
